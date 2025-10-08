42ddc58585e06a9f04b1d79aad8012c49f80157e
Rehan Raja <rehman0rehan@gmail.com>
2025-10-08 20:33:04 +0500

    change

--- 3d-portfolio-main/src/components/models/hero_models/MobileHero.jsx
+++ 3d-portfolio-main/src/components/models/hero_models/MobileHero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const MobileHero = () => {
   return (
-    <div>
+    <div className='mt-[250px]'>
       <img src="https://thumbs.dreamstime.com/b/realistic-d-modern-illustration-room-interior-desk-seat-computer-table-empty-office-home-monochrome-colors-315126995.jpg?w=992" alt="" />
     </div>
   )
